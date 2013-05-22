109f83e56080c73cc49bc37fc0189fc18fbc4c0c
IceDragon200 <mistdragon100@gmail.com>
2013-05-21 19:05:22 -0500

    Gem-fication complete

--- lib/sadie/reaktors.rb
+++ lib/sadie/reaktors.rb
@@ -3,7 +3,14 @@
 #   by IceDragon
 #   dc 11/03/2013
 #   dm 21/05/2013
-dir = File.dirname(__FILE__)
-Dir.glob(File.join(dir, 'reaktors', '*.rb')).each do |fn|
-  require fn
-end
+require 'sadie/reaktor/callback'
+require 'sadie/reaktor/capacitor'
+require 'sadie/reaktor/counter'
+require 'sadie/reaktor/drain'
+require 'sadie/reaktor/emitter'
+require 'sadie/reaktor/floodgate'
+require 'sadie/reaktor/fuse'
+require 'sadie/reaktor/multiplex'
+require 'sadie/reaktor/relay'
+require 'sadie/reaktor/seven_segment'
+require 'sadie/reaktor/two_way_switch'
