60a1da8d9747e319fc5c359b1e0bd52d8f0251e4
IceDragon200 <mistdragon100@gmail.com>
2014-03-05 08:47:04 -0500

    Updated RASM examples

--- examples/rasm/rasm_example01.rasm.rb
+++ examples/rasm/rasm_example01.rasm.rb
@@ -1,6 +1,10 @@
-jmp :main
-label :main
-mov :a, 0
-adi :a, 2
-mov :b, :a
-hlt
+namespace "Program::Main" do
+  jmp_to_label :main
+  label :main do
+    mov :a, 0
+    adi :a, 2
+    mov :b, :a
+    hlt
+  end
+end
+jmp_to_label "Program::Main.main"
---
+++ examples/rasm/rasm_example02.rasm.rb
@@ -0,0 +1,37 @@
+namespace "SASM" do
+  label "set0" do
+    mvi :a, 0
+    ret
+  end
+  label "set1" do
+    mvi :a, 1
+    ret
+  end
+  label "set2" do
+    mvi :a, 2
+    ret
+  end
+end
+namespace "SASM::Program" do
+  label "prog_entry" do
+    mvi :b, 22
+    ret
+  end
+end
+# exposes all labels in the namespace SASM::Program to the current level
+using "SASM::Program"
+# tells the assembler that this instruction is outside the current sourcefile
+extern "SASM::Helper.blak"
+label "something" do
+  nop # does nothing
+end
+namespace "SASM::Program" do
+  label "main" do
+    mvi :a, 0   # ensure that the accumulator is empty
+    adi 2       # add 2 to the accumulator
+    mov :b, :a  # move the value of the accumulator to the
+    ret         # return I think?
+  end
+end
+jmp "SASM::Program.main"
+hlt
